153dd8fd969c16b81f54f185d0964d917d4c4cc0
Aswath-Senthilkumar <asenth25@asu.edu>
2025-11-18 11:07:22 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import NavBar from './components/NavBar'
+import Viewer from './components/Viewer'
+
+const App = () => {
+  return (
+    <main>
+      <NavBar />
+    </main>
+  )
+}
+
+export default App
