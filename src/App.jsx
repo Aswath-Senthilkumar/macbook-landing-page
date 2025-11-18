0c09734d5cdd9e4995ee0adef5600e92435fb832
Aswath-Senthilkumar <asenth25@asu.edu>
2025-11-18 16:45:09 -0700

    Implemented performance section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Viewer from './components/Viewer'
 import { ScrollTrigger, SplitText } from 'gsap/all'
 import gsap from 'gsap'
 import Showcase from './components/Showcase'
+import Performance from './components/Performance'
+import Features from './components/Features'
+import Highlights from './components/Highlights'
+import Footer from './components/Footer'
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -14,6 +18,10 @@ const App = () => {
       <Hero />
       <Viewer />
       <Showcase />
+      <Performance />
+      <Features />
+      <Highlights />
+      <Footer />
     </main>
   )
 }
