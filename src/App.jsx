0d1d066d3777b0b3f305c185603540628ea30313
Aswath-Senthilkumar <asenth25@asu.edu>
2025-11-18 11:19:24 -0700

    Added hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Hero from './components/Hero'
 import NavBar from './components/NavBar'
 import Viewer from './components/Viewer'
 
@@ -5,6 +6,7 @@ const App = () => {
   return (
     <main>
       <NavBar />
+      <Hero />
     </main>
   )
 }
